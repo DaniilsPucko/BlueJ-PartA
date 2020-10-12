475905c334357025554e16dfaf9c9e404e22b992
Daniils Pucko <mondex123@gmail.com>
2020-10-12 17:11:52 +0100

    Added my name

--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -1,3 +1,6 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.*;
 /**
  * TicketMachine models a ticket machine that issues
  * flat-fare tickets.
@@ -9,7 +12,7 @@
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  * 
- * Modified by Student Name
+ * Modified by Daniils Pucko
  */
 public class TicketMachine
 {
@@ -19,25 +22,46 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
-
+    //The arraylist for the all tickets.
+    private ArrayList<Ticket> tickets;
+    
+    
+    private Date today = new Date();
+    //SimpleDateFormat
+    private SimpleDateFormat formatter = new SimpleDateFormat("dd/MMM/yyyy");
+    //Tickets date
+    String date = formatter.format(today);
+    
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine()
     {
-        price = cost;
         balance = 0;
         total = 0;
+        tickets = new ArrayList<Ticket>();
     }
-
+    
     /**
-     * @Return The price of a ticket.
+     * Add a ticket to ticket machine.
      */
-    public int getPrice()
+    public void addTicket(Ticket newTicket)
     {
-        return price;
+        tickets.add(newTicket);
     }
-
+    
+    /**
+     * Print available tickets.
+     */
+    public void printList()
+    {
+        for(Ticket ticket : tickets)
+        {
+            ticket.print();
+        }
+    }
+    
+    
     /**
      * Return The amount of money already inserted for the
      * next ticket.
@@ -69,27 +93,27 @@ public class TicketMachine
      * reduce the current balance by the ticket price. Print
      * an error message if more money is required.
      */
-    public void printTicket()
+    public void buyTicket(Ticket ticket)
     {
-        if(balance >= price) 
+        if(balance >=ticket.price) 
         {
             // Simulate the printing of a ticket.
             System.out.println("##################");
-            System.out.println("# The BlueJ Line");
-            System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
+            System.out.println("Ticket destination: " + ticket.destination);
+            System.out.println("Ticket price: " + ticket.price + " pennies");
+            System.out.println("Date: " + date);
             System.out.println("##################");
             System.out.println();
 
             // Update the total collected with the price.
-            total = total + price;
+            total = total + ticket.price;
             // Reduce the balance by the price.
-            balance = balance - price;
+            balance = balance - ticket.price;
         }
         else 
         {
             System.out.println("You must insert at least: " +
-                               (price - balance) + " more cents.");
+                               (ticket.price - balance) + " more cents.");
                     
         }
     }
