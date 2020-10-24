a831a2a56ae017588e7e7c21be3c219c1cc96354
Daniils Pucko <mondex123@gmail.com>
2020-10-24 16:11:59 +0100

    Update Course.java

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class Course here.
  * 
@@ -8,26 +8,13 @@
 public class Course
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private ArrayList<Module> modules;
 
     /**
      * Constructor for objects of class Course
      */
     public Course()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        
     }
 }
