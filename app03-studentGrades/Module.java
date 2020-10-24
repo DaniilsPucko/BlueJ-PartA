40dceb4f53e9169520d67c5a62dfed3d3d3430af
Daniils Pucko <mondex123@gmail.com>
2020-10-24 16:33:39 +0100

    Updated module class

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -8,26 +8,17 @@
 public class Module
 {
     // instance variables - replace the example below with your own
-    private int x;
+    public String title;
+    public int code;
 
     /**
      * Constructor for objects of class Module
      */
-    public Module()
+    public Module(String fullTitle, int fullCode)
     {
-        // initialise instance variables
-        x = 0;
+        title = fullTitle;
+        code = fullCode;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
