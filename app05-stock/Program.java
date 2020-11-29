bf27061cc722b001803c45267f335f72df3eb127
Daniils Pucko <mondex123@gmail.com>
2020-11-29 21:27:53 +0000

    Update Program.java

--- app05-stock/Program.java
+++ app05-stock/Program.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Program here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Daniils Pucko)
+ * @version (29.11.2020)
  */
 public class Program
 {
