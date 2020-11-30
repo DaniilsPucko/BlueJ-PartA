5af71fcdfb3c8d896b8c486a4e878b1742269e29
Daniils Pucko <mondex123@gmail.com>
2020-11-30 14:10:38 +0000

    Update StockManager.java

--- app05-stock/StockManager.java
+++ app05-stock/StockManager.java
@@ -26,7 +26,17 @@ public class StockManager
      */
     public void addProduct(Product item)
     {
-        stock.add(item);
+        int id = item.getID();
+        for(Product product : stock) {
+            if (product.getID()==id)
+            {
+                System.out.println("Cannot add product, because product with same ID exists.");
+            }
+            else
+            {
+                stock.add(item);
+            }
+        }
     }
     
     /**
