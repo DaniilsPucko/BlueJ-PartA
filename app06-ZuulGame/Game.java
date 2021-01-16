7a31d932d2261698ea94ffd3f800a5eb9557ab15
Daniils Pucko <mondex123@gmail.com>
2021-01-16 14:54:29 +0000

    Update Game.java

--- app06-ZuulGame/Game.java
+++ app06-ZuulGame/Game.java
@@ -81,9 +81,6 @@ public class Game
     public void play() 
     {            
         printWelcome();
-
-        // Enter the main command loop.  Here we repeatedly read commands and
-        // execute them until the game is over.
                 
         boolean finished = false;
         
