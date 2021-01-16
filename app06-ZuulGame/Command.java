677106c005027433cb9a85e53d4eefe5d9abd88e
Daniils Pucko <mondex123@gmail.com>
2021-01-16 15:41:43 +0000

    Update Command.java

--- app06-ZuulGame/Command.java
+++ app06-ZuulGame/Command.java
@@ -54,14 +54,6 @@ public class Command
     }
 
     /**
-     * @return true if this command was not understood.
-     */
-    public boolean isUnknown()
-    {
-        return (commandWord == CommandWord.UNKNOWN);
-    }
-
-    /**
      * @return true if the command has a second word.
      */
     public boolean hasSecondWord()
