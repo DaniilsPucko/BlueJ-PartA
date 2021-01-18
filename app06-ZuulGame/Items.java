202d52ba2ba3520da03cc46347f85edc7e5b6a80
Daniils Pucko <mondex123@gmail.com>
2021-01-18 16:06:15 +0000

    Update Items.java

--- app06-ZuulGame/Items.java
+++ app06-ZuulGame/Items.java
@@ -8,26 +8,39 @@
 public class Items
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String name;
+    private String description;
 
     /**
-     * Constructor for objects of class Items
+     * Constructor for objects of class Item
      */
-    public Items()
+    public Items(String n, String d)
     {
-        // initialise instance variables
-        x = 0;
+        name = n;
+        description = d;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Prints name of the item;
      */
-    public int sampleMethod(int y)
+    public String getName()
     {
-        // put your code here
-        return x + y;
+        return name;
+    }
+    
+    /**
+     * Prints info about item;
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    /**
+     * Prints item name with description
+     */
+    public void printItem()
+    {
+        System.out.println(name +" - "+  description);
     }
 }
