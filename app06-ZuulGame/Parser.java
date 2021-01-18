4ff673c17e4f96966784c54e873b34520c168652
Daniils Pucko <mondex123@gmail.com>
2021-01-18 15:38:15 +0000

    Update Parser.java

--- app06-ZuulGame/Parser.java
+++ app06-ZuulGame/Parser.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
  */
 public class Parser 
 {
-    private CommandWords commands;  // holds all valid command words
+
     private Scanner reader;         // source of command input
 
     /**
@@ -26,45 +26,8 @@ public class Parser
      */
     public Parser() 
     {
-        commands = new CommandWords();
         reader = new Scanner(System.in);
     }
 
-    /**
-     * @return The next command from the user.
-     */
-    public Command getCommand() 
-    {
-        String inputLine;   // will hold the full input line
-        String word1 = null;
-        String word2 = null;
 
-        System.out.print("> ");     // print prompt
-
-        inputLine = reader.nextLine();
-
-        // Find up to two words on the line.
-        Scanner tokenizer = new Scanner(inputLine);
-        
-        if(tokenizer.hasNext()) 
-        {
-            word1 = tokenizer.next();      // get first word
-        
-            if(tokenizer.hasNext()) 
-            {
-                word2 = tokenizer.next();      // get second word
-                // note: we just ignore the rest of the input line.
-            }
-        }
-
-        return new Command(commands.getCommandWord(word1), word2);
-    }
-
-    /**
-     * Print out a list of valid command words.
-     */
-    public void showCommands()
-    {
-        commands.showAll();
-    }
 }
