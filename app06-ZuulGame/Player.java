ce80399b881ad4371d58be9069c604d03f3d876d
Daniils Pucko <mondex123@gmail.com>
2021-01-18 16:06:20 +0000

    Update Player.java

--- app06-ZuulGame/Player.java
+++ app06-ZuulGame/Player.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class Player here.
  * 
@@ -8,26 +8,108 @@
 public class Player
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int health;
+    private int score;
+    private String name;
+    private ArrayList<Items> inventory;
 
     /**
      * Constructor for objects of class Player
      */
-    public Player()
+    public Player(String name1)
     {
-        // initialise instance variables
-        x = 0;
+        this.name = name1;
+        score = 0;
+        health = 100;
+        inventory = new ArrayList<>();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Prints score of the player
      */
-    public int sampleMethod(int y)
+    public int getScore()
     {
-        // put your code here
-        return x + y;
+        return score;
+    }
+    
+    /**
+     * Prints health of the player
+     */
+    public int getHealth()
+    {
+        return health;
+    }
+    
+    /**
+     * Prints name of the player
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     * Method which affects player's health
+     */
+    public int affectHealth(int h)
+    {
+        health = health + h;
+        return health;
+    }
+    
+    /**
+     * Method which affects player's score
+     */
+    public int affectScore(int s)
+    {
+        score = score + s;
+        return score;
+    }
+    
+    /**
+     * Remove item from players inventory
+     */
+    public Items removeItem(String name)
+    {
+        for(Items item : inventory){
+            if (item.getName().contains(name)){
+                inventory.remove(item);
+                return item;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Add item to player's inventory
+     */
+    public void addItem(Items item)
+    {
+        inventory.add(item);
+    }
+    
+    /**
+     * Checks inventory for an item
+     */
+    public boolean findItem(String name)
+    {
+        for(Items item : inventory){
+            if (item.getName().contains(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Prints all available items in the inventory
+     */
+    public void printItems()
+    {
+        for(Items item : inventory) {
+            item.printItem();
+        }
     }
 }
+
+
